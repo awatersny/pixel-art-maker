533d52d060fe52119e99d2772ddbb62375507800
awatersny <awatersny@gmail.com>
2022-12-15 21:12:11 -0500

    Use array instead of matrix for target ids

--- js/script.js
+++ js/script.js
@@ -2,25 +2,32 @@
 
 /*----------------------------------------Variables------------------------------------------*/
 let grid = [];
+let height = 1;
+let width = 1;
 
 /*--------------------------------Cached Element References----------------------------------*/
 const sizePicker = document.getElementById("sizePicker");
+const colorPicker = document.getElementById("colorPicker");
+const pixelCanvas = document.getElementById("pixelCanvas");
 const pixelCells = document.getElementsByClassName("pixelCell");
 
 /*------------------------------------Event Listeners----------------------------------------*/
 sizePicker.addEventListener("submit", generateGrid);
+pixelCanvas.addEventListener("click", colorCell);
 
 /*---------------------------------------Functions-------------------------------------------*/
 function generateGrid(evt) {
   console.log()
-  let height = evt.target[0].value;
-  let width = evt.target[1].value;
-  let row = [];
-  for(let col = 0; col < width; col++) {
-    row.push("#fff");
+  height = evt.target[0].value;
+  width = evt.target[1].value;
+  let pixels = height * width;
+  for(let i = 0; i < pixels; i++) {
+    grid.push("#fff");
   }
-  for(let i = 0; i < height; i++) {
-    grid.push(row);
-    console.log(grid[i]);
-  }
+  console.log(grid);
+}
+
+function colorCell(evt) {
+  console.log();
+  console.log(evt.target.id);
 }
